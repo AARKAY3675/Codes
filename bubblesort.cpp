0d3dcdce036e05a583e90554861bf19c32224d63
Muskankumari2002 <86469435+Muskankumari2002@users.noreply.github.com>
2021-09-27 18:57:47 +0530

    bubble sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,3 +1,6 @@
+// Bubble sort: In bubble sort we compare the adjacent element if right element is greater than the left element then it will swap otherwise it remain as it is.
+//here is two loop first will run till n-1
+//second loop i.e after every ilteration the last element will be sorted and second loop will be run till n-i-1
 #include<iostream>
 using namespace std;
 int main()
@@ -6,9 +9,10 @@ int main()
     cout<<"Enter the size of array";
     cin>>n;
     int arr[n];
+    cout<<"Enter the elements in an array";
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i];
+         cin>>arr[i];
     }
    for(int i =0;i<n-1;i++)
    {
